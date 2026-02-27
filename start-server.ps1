90dd38410cdb94547b791481645922db07e9289a
Kfir Ozer <ufkfir@icloud.com>
2026-02-27 18:56:15 +0200

    added a start server and browser script for windows

---
+++ start-server.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = "Stop"
+
+$root = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Start the Python HTTP server in this folder
+Start-Process -FilePath "python" -ArgumentList "-m", "http.server", "8000" -WorkingDirectory $root
+
+Start-Sleep -Seconds 1
+
+# Open default browser to index.html
+Start-Process "http://localhost:8000/index.html"
